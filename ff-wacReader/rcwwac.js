34573d3ae82d6c2a4f396522cdebef31446abfd4
ethankale <nathanielkale@gmail.com>
2025-07-06 10:27:42 -0700

    Packaged the reformat loop into a function, to facilitate applying it to a page displaying full chapters, e.g. https://app.leg.wa.gov/RCW/default.aspx?cite=43.21A&full=true#43.21A.732.  Still only works on pages displaying single sections for now.

--- ff-wacReader/rcwwac.js
+++ ff-wacReader/rcwwac.js
@@ -1,56 +1,87 @@
 
-var elements = document.getElementById("contentWrapper").children[2].children;
+/* 
+WAC/RCW is displayed in different ways depending on the browser configuration.
+  Get the browser variable that determines what HTML format is displayed,
+  then pull the appropriate child elements to style.
+*/
 
-// Starts with (1) or similar
-var level1regex = new RegExp('^[(][0-9]+[)]');
-// Starts with (a) or similar
-var level2regex = new RegExp('^[(][a-z][)]');
-// Starts with (i) or similar (lower case roman numeral)
-var level3regex = new RegExp('^[(](xc|xl|l?x{0,3})(ix|iv|v?i{0,3})[)]');
-// Starts with (A) or similar
-var level4regex = new RegExp('^[(][A-Z][)]');
-// Starts with (i) or similar (lower case roman numeral)
-var level5regex = new RegExp('^[(](XC|XL|L?X{0,3})(IX|IV|V?I{0,3})[)]');
+var parameters = new URLSearchParams(window.location.search);
+var URLvar = parameters.get('full')
+console.log(URLvar == null)
 
-var lowerhregex = new RegExp('\\(h\\)');
-var upperHregex = new RegExp('\\(H\\)');
+// If there's a "full" value in the URL, it means displaying many sections of 
+//   RCW or WAC at the same time.
 
-var lastClass   = "level1";
-var lastContent = "";
+if (URLvar == null) {
+  var elements = document.getElementById("contentWrapper").children[2].children;
+  reformat(elements);
+} else {
+  console.log("full variable - displaying a complete chapter")
+};
 
-for (var i = 0, element; element = elements[i++];) {
-  element.style.removeProperty("text-indent");
-  
-  var currentClass = lastClass;
-  
-  if (level1regex.test(element.innerHTML)) {
-    currentClass = "level1";
-  
-  // Element 3 comes first, because otherwise (i) would be classified as
-  //  a lower-case letter (2), rather than a roman numeral (3).
-  } else if (level3regex.test(element.innerHTML)) {
-    if (lowerhregex.test(lastContent)) {
+
+/* 
+Within a section, the HTML is structured the same way, whether it's
+  by itself on the page (no 'full' variable declared) or it's within a
+  <span> on a page with the entire chapter ('full' variable declared).
+
+  This function should work the same as long as it's pointed at the right
+  list of elements.
+*/
+function reformat(elements) {
+
+  // Starts with (1) or similar
+  var level1regex = new RegExp('^[(][0-9]+[)]');
+  // Starts with (a) or similar
+  var level2regex = new RegExp('^[(][a-z][)]');
+  // Starts with (i) or similar (lower case roman numeral)
+  var level3regex = new RegExp('^[(](xc|xl|l?x{0,3})(ix|iv|v?i{0,3})[)]');
+  // Starts with (A) or similar
+  var level4regex = new RegExp('^[(][A-Z][)]');
+  // Starts with (i) or similar (lower case roman numeral)
+  var level5regex = new RegExp('^[(](XC|XL|L?X{0,3})(IX|IV|V?I{0,3})[)]');
+
+  var lowerhregex = new RegExp('\\(h\\)');
+  var upperHregex = new RegExp('\\(H\\)');
+
+  var lastClass   = "level1";
+  var lastContent = "";
+
+  for (var i = 0, element; element = elements[i++];) {
+    element.style.removeProperty("text-indent");
+    
+    var currentClass = lastClass;
+    
+    if (level1regex.test(element.innerHTML)) {
+      currentClass = "level1";
+    
+    // Element 3 comes first, because otherwise (i) would be classified as
+    //  a lower-case letter (2), rather than a roman numeral (3).
+    } else if (level3regex.test(element.innerHTML)) {
+      if (lowerhregex.test(lastContent)) {
+        currentClass = "level2";
+      } else {
+        currentClass = "level3";
+      };
+    } else if (level2regex.test(element.innerHTML)) {
       currentClass = "level2";
-    } else {
-      currentClass = "level3";
+    
+    // Same as 3 and 2 above.
+    } else if (level5regex.test(element.innerHTML)) {
+      currentClass = "level5";
+    } else if (level4regex.test(element.innerHTML)) {
+      currentClass = "level4";
     };
-  } else if (level2regex.test(element.innerHTML)) {
-    currentClass = "level2";
-  
-  // Same as 3 and 2 above.
-  } else if (level5regex.test(element.innerHTML)) {
-    currentClass = "level5";
-  } else if (level4regex.test(element.innerHTML)) {
-    currentClass = "level4";
-  };
-  
-  console.log(lastContent);
-  console.log(element.innerHTML);
-  console.log(currentClass);
-  console.log('----');
-  
-  element.className = currentClass;
-  lastClass   = currentClass;
-  lastContent = element.innerHTML;
-  
-}
+    
+    console.log(lastContent);
+    console.log(element.innerHTML);
+    console.log(currentClass);
+    console.log('----');
+    
+    element.className = currentClass;
+    lastClass   = currentClass;
+    lastContent = element.innerHTML;
+    
+  }
+};
+
